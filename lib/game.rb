37fbbc3112d43571797e80138e3adac473bb3fc4
Connor Worthington <worthington.connor@gmail.com>
2020-09-18 15:35:36 +0100

    changes switch method

--- lib/game.rb
+++ lib/game.rb
@@ -2,20 +2,23 @@ require_relative 'player.rb'
 
 class Game 
 
- attr_reader :player_1, :player_2, :turn
+ attr_reader :player_1, :player_2, :active, :player
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
-    @turn = player_1 
+    @player = [@player_1, @player_2]
+    @active = @player[0]
   end 
 
-  def attack(player) 
-    player.hit
+  def attack 
+    @player[1].hit
+    switch
   end 
 
   def switch 
-    @turn == player_1 ? @turn = player_2 : @turn = player_1
+    @active == @player_1 ? @player.reverse! : @player
+    @player[0]
   end 
 
 end 
